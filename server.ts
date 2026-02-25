59e3d56ef69a4e823723239973a02fc060795537
lircano24 <lircano24@gmail.com>
2026-02-25 23:35:44 +0100

    fix: replace Express 5 path wildcard with manual middleware

--- server.ts
+++ server.ts
@@ -300,8 +300,12 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     app.use(express.static("dist"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve("dist", "index.html"));
+    app.use((req, res, next) => {
+      if (req.method === "GET") {
+        res.sendFile(path.resolve("dist", "index.html"));
+      } else {
+        next();
+      }
     });
   }
 
